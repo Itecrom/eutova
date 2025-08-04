8a7c9c6f51b244cd826d53cab6d8d9fc8f241c82
eunicekaiya <eunicekaiya789@gmail.com>
2025-08-04 10:59:28 +0200

    Register Admin code

---
+++ authentication/register.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Register</title>
+</head>
+<body>
+    
+<div>
+    <h1>Admin Registration</h1>
+    <form action="register.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <button type="submit">Submit</button>
+        <p>Already registered? Login <a href="login.php"></a> </p>
+    </form>
+</div>
+
+</body>
+</html>
